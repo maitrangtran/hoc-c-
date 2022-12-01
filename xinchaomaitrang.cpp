53b001a4c09da4d6e56d61acc6eda60a41946750
maitrangtran <119572137+maitrangtran@users.noreply.github.com>
2022-12-01 16:05:05 +0700

    Add files via upload

---
+++ xinchaomaitrang.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+//day la khai bao khong gian lam viec
+int main()
+{
+/*
+dong1
+dong2
+dong3
+*/
+
+	cout<<"xin chao mai trang tran";
+	cout<<"truong thpt luong son";
+
+	return 0;
+}
