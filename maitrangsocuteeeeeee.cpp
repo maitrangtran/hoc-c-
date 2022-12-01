db5911ffd01de1586db7567f3923d5adc4e63b44
maitrangtran <119572137+maitrangtran@users.noreply.github.com>
2022-12-01 16:04:14 +0700

    Add files via upload

---
+++ maitrangsocuteeeeeee.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	a=100;
+	b=50;
+	c=150;
+	cout<<"tong 3 so ="<<a+b+c<<endl;
+	cout<<"hieu 3 so ="<<a-b-c<<endl;
+	cout<<"tich 3 so ="<<a*b*c<<endl;
+	cout<<"thuong 3 so ="<<a/b/c<<endl;
+	return 0;
+}
